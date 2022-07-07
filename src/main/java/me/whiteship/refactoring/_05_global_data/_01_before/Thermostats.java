0dc49f10f40a313b04e245d80d41591b6803f0f1
sungmin69355 <sungmin69355@naver.com>
2022-07-07 21:50:14 +0900

    변수 캡슐화 하기

--- src/main/java/me/whiteship/refactoring/_05_global_data/_01_before/Thermostats.java
+++ src/main/java/me/whiteship/refactoring/_05_global_data/_01_before/Thermostats.java
@@ -2,14 +2,45 @@ package me.whiteship.refactoring._05_global_data._01_before;
 
 public class Thermostats {
 
-    public static Integer targetTemperature = 70;
+    private static Integer targetTemperature = 70;
 
-    public static Boolean heating = true;
+    private static Boolean heating = true;
 
-    public static Boolean cooling = false;
+    private static Boolean cooling = false;
 
-    public static Boolean fahrenheit = true;
+    private static Boolean fahrenheit = true;
 
+    public static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
 
+    public static void setTargetTemperature(Integer targetTemperature) {
+        Thermostats.targetTemperature = targetTemperature;
+    }
 
+    public static Boolean getHeating() {
+        return heating;
+    }
+
+    public static void setHeating(Boolean heating) {
+        // TODO validation
+        Thermostats.heating = heating;
+        // TODO notify
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static void setCooling(Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static Boolean getFahrenheit() {
+        return fahrenheit;
+    }
+
+    public static void setFahrenheit(Boolean fahrenheit) {
+        Thermostats.fahrenheit = fahrenheit;
+    }
 }
