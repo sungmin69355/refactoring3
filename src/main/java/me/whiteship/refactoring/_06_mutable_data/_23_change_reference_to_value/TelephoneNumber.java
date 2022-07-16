517b17b93d7f5007d62c6aa8b0644eacfbb071fc
sungmin69355 <sungmin69355@naver.com>
2022-07-16 21:08:51 +0900

    Change Reference to Value

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
@@ -1,24 +1,39 @@
 package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
 
+import java.util.Objects;
+
 public class TelephoneNumber {
 
-    private String areaCode;
+    private final String areaCode;
 
-    private String number;
+    private final String number;
+
+    public TelephoneNumber(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
 
     public String areaCode() {
         return areaCode;
-    }
-
-    public void areaCode(String areaCode) {
-        this.areaCode = areaCode;
     }
 
     public String number() {
         return number;
     }
 
-    public void number(String number) {
-        this.number = number;
+
+    //레퍼런스의 동일성을 확인한다.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TelephoneNumber that = (TelephoneNumber) o;
+        return Objects.equals(areaCode, that.areaCode) && Objects.equals(number, that.number);
+    }
+
+    //equals와 함께 만들어줘야한다. 해시셋을 같이 검증해줘야한다.
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
     }
 }
