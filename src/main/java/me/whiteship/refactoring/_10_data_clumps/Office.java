c88e1ba5a628863e0cc4c08428131d2738470ba7
sungmin69355 <sungmin69355@naver.com>
2022-09-21 20:55:15 +0900

    data clumps -> 클래스 추출하여 개선

--- src/main/java/me/whiteship/refactoring/_10_data_clumps/Office.java
+++ src/main/java/me/whiteship/refactoring/_10_data_clumps/Office.java
@@ -4,33 +4,19 @@ public class Office {
 
     private String location;
 
-    private String officeAreCode;
+    private TelephoneNumber officePhoneNumber;
 
-    private String officeNumber;
-
-    public Office(String location, String officeAreCode, String officeNumber) {
+    public Office(String location, TelephoneNumber officePhoneNumber) {
         this.location = location;
-        this.officeAreCode = officeAreCode;
-        this.officeNumber = officeNumber;
+        this.officePhoneNumber = officePhoneNumber;
     }
 
     public String officePhoneNumber() {
-        return officeAreCode + "-" + officeNumber;
+        return officePhoneNumber.toString();
     }
 
-    public String getOfficeAreCode() {
-        return officeAreCode;
+    public TelephoneNumber getOfficePhoneNumber() {
+        return officePhoneNumber;
     }
 
-    public void setOfficeAreCode(String officeAreCode) {
-        this.officeAreCode = officeAreCode;
-    }
-
-    public String getOfficeNumber() {
-        return officeNumber;
-    }
-
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
-    }
 }
