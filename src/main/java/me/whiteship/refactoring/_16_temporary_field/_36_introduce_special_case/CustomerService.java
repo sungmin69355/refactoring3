bcd8d4751834b5a541e9ec236ba7db09ff48a090
sungmin69355 <sungmin69355@naver.com>
2022-10-04 20:49:49 +0900

    Null Object 패턴 적용

--- src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/CustomerService.java
+++ src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/CustomerService.java
@@ -3,26 +3,15 @@ package me.whiteship.refactoring._16_temporary_field._36_introduce_special_case;
 public class CustomerService {
 
     public String customerName(Site site) {
-        Customer customer = site.getCustomer();
-
-        String customerName;
-        if (customer.getName().equals("unknown")) {
-            customerName = "occupant";
-        } else {
-            customerName = customer.getName();
-        }
-
-        return customerName;
+        return site.getCustomer().getName(); //이미 이 부분에서 unknown인지 판별
     }
 
     public BillingPlan billingPlan(Site site) {
-        Customer customer = site.getCustomer();
-        return customer.getName().equals("unknown") ? new BasicBillingPlan() : customer.getBillingPlan();
+        return site.getCustomer().getBillingPlan();
     }
 
     public int weeksDelinquent(Site site) {
-        Customer customer = site.getCustomer();
-        return customer.getName().equals("unknown") ? 0 : customer.getPaymentHistory().getWeeksDelinquentInLastYear();
+        return site.getCustomer().getPaymentHistory().getWeeksDelinquentInLastYear();
     }
 
 }
